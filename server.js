700ee53e89d755abed501a3eaa0f8ddef3289919
RBennyjay <ejohn1@byupathway.edu>
2025-07-08 01:13:34 +0100

    initial commit

--- server.js
+++ server.js
@@ -6,21 +6,36 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
 
+//index route
+app.get("/", function  (req, res){
+  res.render("index", { title: "Home" })
+})
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 5500 // fallback for local testing
+const host = process.env.HOST || "localhost" // fallback for local testing
 
 /* ***********************
  * Log statement to confirm server operation
