c0bc4eec015ec06f18ac4a5e83e0e824b859c9a7
RBennyjay <ejohn1@byupathway.edu>
2025-07-20 01:03:09 +0100

    completed - Robust Error Handling

--- server.js
+++ server.js
@@ -32,7 +32,10 @@ app.use(static)
 // app.get("/", function  (req, res){
 //   res.render("index", { title: "Home" })
 // })
-app.get("/", baseController.buildHome)
+// app.get("/", baseController.buildHome)
+
+// Index route
+app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
@@ -47,12 +50,17 @@ app.use(async (req, res, next) => {
 * Express Error Handler
 * Place after all other middleware
 *************************/
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
+    message,
     nav
   })
 })
